7f01330b5562fe96fca90df8c67e5ffdf9ea241f
capitalrisecyber <islamicarmx@gmail.com>
2026-07-02 13:58:05 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBxIx1bUqIhB7FzsgEtEw1CWmYLWjby0xI",
+  authDomain: "capitalrise-a1ce7.firebaseapp.com",
+  projectId: "capitalrise-a1ce7",
+  storageBucket: "capitalrise-a1ce7.firebasestorage.app",
+  messagingSenderId: "491288482764",
+  appId: "1:491288482764:web:fbb72f2ad8adecdba74369"
+};
+
+firebase.initializeApp(firebaseConfig);
